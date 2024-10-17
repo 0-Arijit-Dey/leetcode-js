05661e0777e4af8d1c1544fe41870b50078d7640
0-Arijit-Dey <aeiouxyz0001@gmail.com>
2024-10-17 11:57:49 +0000

    easy

---
+++ EASY/09_transform_over_element.js
@@ -0,0 +1,21 @@
+// 2635. Apply Transform Over Each Element in Array
+const arr = [1, 2, 3];
+const plusone = (n) => n + 1;
+const plusI = (n, i) => n + i;
+const constant = () => 42;
+
+var map = function (arr, fn) {
+  let modefiedArr = [];
+  arr.forEach((element, i) => {
+    modefiedArr.push(fn(element, i));
+  });
+  return modefiedArr;
+};
+
+const newArray1 = map(arr, plusone);
+const newArray2 = map(arr, plusI);
+const newArray3 = map(arr, constant);
+
+console.log(newArray1); // [ 2, 3, 4 ]
+console.log(newArray2); // [ 1, 3, 5 ]
+console.log(newArray3); // [ 42, 42, 42 ]
